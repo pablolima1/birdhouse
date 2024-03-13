b0b22a2d3024d9316fcbd1e740269abffe297925
Pablo Lima <pablolimati@gmail.com>
2024-03-12 23:00:28 -0300

    Traduzindo o search do filtro da sidebar

--- config/adminlte.php
+++ config/adminlte.php
@@ -304,7 +304,7 @@ return [
         // Sidebar items:
         [
             'type' => 'sidebar-menu-search',
-            'text' => 'search',
+            'text' => 'Buscar',
         ],
         [
             'text' => 'blog',
